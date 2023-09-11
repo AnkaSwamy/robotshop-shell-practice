2c6c3e3facf25c0632cb144cc0d04eca15aab8a8
Anka swamy <ankaswamy.telagorla@gmail.com>
2023-09-11 16:21:08 +0530

    frontend and roboshop.conf checking it's working or not

--- frontend.sh
+++ frontend.sh
@@ -1,27 +1,27 @@
 
 
-echo -e "\e[31m Installing Nginx server \e[0m "
-yum install nginx -y &>>$log_file
+echo -e "\e[31m Installing Nginx server \e[0m"
+yum install nginx -y &>>/tmp/roboshop.log
 
 
 echo -e "\e[31m Remove default content \e[0m"
-rm -rf /usr/share/nginx/html/* &>>$log_file
+rm -rf /usr/share/nginx/html/* &>>/tmp/roboshop.log
 mkdir /app
 
 
 echo -e "\e[31m Download frontend content \e[0m"
-curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip  &>>$log_file
+curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip  &>>/tmp/roboshop.log
 
 
 echo -e "\e[31m Extract frontend content \e[0m"
 cd /usr/share/nginx/html
-unzip /tmp/frontend.zip &>>$log_file
+unzip /tmp/frontend.zip &>>/tmp/roboshop.log
 
 
 echo -e "\e[31m Update the configuration \e[0m"
-cp /root/robotshop-shell-practice/roboshop.conf /etc/nginx/default.d/roboshop.conf  &>>$log_file
+cp /root/robotshop-shell-practice/roboshop.conf /etc/nginx/default.d/roboshop.conf  &>>/tmp/roboshop.log
 
 
 echo -e "\e[31m Start nginx server \e[0m"
-systemctl enable nginx &>>$log_file
-systemctl restart nginx  &>>$log_file
+systemctl enable nginx &>>/tmp/roboshop.log
+systemctl restart nginx  &>>/tmp/roboshop.log
